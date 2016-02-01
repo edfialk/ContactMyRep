d1b3f56d61ffbede3cf3a5a8f344858216c70dad
vagrant <edfialk@gmail.com>
2016-02-01 18:46:58 +0000

    fix login path

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,6 +23,9 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
+    protected $loginPath = '/login';
+    protected $redirectPath = '/';
+
     /**
      * Create a new authentication controller instance.
      *
