23cea532e2a9c72ad417217cd696527f30583b7f
vagrant <edfialk@gmail.com>
2016-03-03 18:40:14 +0000

    google verification

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Contact My Reps</title>
+	<title>@yield('title', 'Contact My Reps')</title>
 
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<meta name="description" content="@yield('description')">
+	<meta name="google-site-verification" content="AE_XVi6xkk5SCzCBGF_nUkmWOW5GNZRnB7IaaRLPxPI" />
 
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
